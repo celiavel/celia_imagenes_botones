c2897efe49ed0ca2305c27f08f58835022d44897
Celia Velert <celiavelertsoto@gmail.com>
2018-11-26 17:57:05 +0100

    initial commit

---
+++ AppCelia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCelia
+//
+//  Created by Celia on 26/11/2018.
+//  Copyright © 2018 Celia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
